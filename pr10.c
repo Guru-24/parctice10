18ac0aa56d7f88968b0115012f12b019f540fcfd
Guru-24 <guru.g@s.amity.edu>
2025-07-27 23:30:44 +0530

    added for loop

--- pr10.c
+++ pr10.c
@@ -5,3 +5,9 @@ int main() {
     int i;
     float data[10];
     printf("Enter 10 elements: ");
+    for (i = 0; i < 10; ++i)
+        scanf("%f", &data[i]);
+    printf("\nStandard Deviation = %.6f", calculateSD(data));
+    return 0;
+}
+
