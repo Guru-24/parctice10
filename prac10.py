5a531291d2860ac3574c471c8e3da71f87e226b9
saikrishna070605 <bandaru.saikrishna@s.amity.edu>
2025-07-28 12:30:16 +0530

    added

--- prac10.py
+++ prac10.py
@@ -3,9 +3,14 @@ if x > y:
         smaller = y
         else:
         smaller = x
+        else
+        biger = z`
          for i in range(1, smaller+1):
         if((x % i == 0) and (y % i == 0)):
             hcf = i
     return hcf
 num1 = 54
 num2 = 24
+num3 = 56
+num4 = 60
+
